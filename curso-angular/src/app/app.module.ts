786d292b14831ce2e97078570d8abcdf38ddda63
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2022-09-27 11:58:49 -0300

    Criando components dentro da pasta de component com ng generate component components/<nome>

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentsComponent } from './components/first-components/first-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentsComponent
   ],
   imports: [
     BrowserModule,
