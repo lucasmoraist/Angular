baa0a4233d3d897c9f9329701351bbcaf5bf857f
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2022-09-28 08:49:11 -0300

    Interpolando os dados do arquivo ts para o arquivo html

--- curso-angular/src/app/components/first-components/first-components.component.ts
+++ curso-angular/src/app/components/first-components/first-components.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentsComponent implements OnInit {
 
+  name: string = 'Lucas';
+  age: number = 18;
+  job = "Programador"; //Estes dados irão vir do banco de dados
+  hobbies = ["Jogar", "Sair", "Viajar"]
+  car = {
+    name: "BMW X1",
+    year: 2020
+  }
+
   constructor() { }
 
   ngOnInit(): void {
