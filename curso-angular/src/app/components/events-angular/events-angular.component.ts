93fe2d990ad27675d2d4a61987542ab7d6e4ce8a
lucasmoraisnt <rm93267@fiap.com.br>
2022-10-20 13:24:19 -0300

    Testando os eventos no angular

--- curso-angular/src/app/components/events-angular/events-angular.component.ts
+++ curso-angular/src/app/components/events-angular/events-angular.component.ts
@@ -9,9 +9,27 @@ export class EventsAngularComponent implements OnInit {
 
   @Input() titulo!: {events: String};
 
+  num: number = 2;
+  mult(): void{
+    this.num = this.num * 2;
+  }
+  soma(): void{
+    this.num = this.num + 2;
+  }
+  div(): void{
+    this.num = this.num / 2;
+  }
+  sub(): void{
+    this.num = this.num - 2;
+  }
+
+  show: boolean = true;
+  showMessage(): void{
+    this.show = !this.show; //!this.show ou toggle, ele faz a inversão dos valores, ou seja, !this.xxxx irá sempre inverter o valor
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
